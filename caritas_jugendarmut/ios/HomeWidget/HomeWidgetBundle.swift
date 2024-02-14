2c5df246fabef62c1cc92fd7fa4605350c1e44cb
Jan <jan.schiffer@hsw-stud.de>
2024-02-14 20:25:54 +0100

    simple widget for ios

---
+++ caritas_jugendarmut/ios/HomeWidget/HomeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HomeWidgetBundle.swift
+//  HomeWidget
+//
+//  Created by Jan Schiffer on 08.02.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HomeWidget()
+    }
+}
